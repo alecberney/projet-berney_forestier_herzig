cdbd57de7635f64e0f8985bad59e39582119fa41
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-03-04 19:22:56 +0100

    Ajout Main pour Picocli
    La classe main pour picoCli a été ajouté ainsi que la première commande

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
+
+/**
+ * Class implémentant Main pour picoCLI
+ * @author Herzig Melvyn
+ */
+@CommandLine.Command(
+        name = "mainCmd"
+)
+public class Main implements Callable<Integer>
+{
+   /**
+    * Méthode pour l'appel de la commande mainCmd avec picoCLI
+    */
+      @Override
+   public Integer call() throws Exception
+   {
+
+      System.out.println("Hello world!");
+
+      return 0;
+   }
+
+   public static void main(String[] args)
+   {
+      int exitCode = new CommandLine(new Main()).execute(args);
+      System.exit(exitCode);
+   }
+}
---
+++ src/test/java/gen/MainTest.java
@@ -0,0 +1,20 @@
+package gen;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Classe de test pour la classe Main
+ */
+public class MainTest
+{
+   /**
+    * Test de la méthode run.
+    */
+   @Test
+   void runTest()
+   {
+      //Dumb test to export folder structure
+      assertEquals(1,1);
+   }
+}
