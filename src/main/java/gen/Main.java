f3b8ad0b7b9f3a3775e0493cb3aab2bcce4d4ed2
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-03-04 20:17:03 +0100

    Ajout de la commande new
    La commande new a été jouté. pour le moment elle n'a pas d'implémentation

--- src/main/java/gen/Main.java
+++ src/main/java/gen/Main.java
@@ -25,6 +25,15 @@ public class Main implements Callable<Integer>
       return 0;
    }
 
+   /**
+    * Méthode pour l'appel de la commande mainCmd avec picoCLI
+    */
+   @CommandLine.Command(name = "new", description = "command new from mainCmd")
+   public Integer newCmd() throws Exception
+   {
+      return 0;
+   }
+
    public static void main(String[] args)
    {
       int exitCode = new CommandLine(new Main()).execute(args);
