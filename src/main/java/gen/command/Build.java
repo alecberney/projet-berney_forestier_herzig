f6a2a4a88d59149938619f0e18c8432db688645e
Quentin Forestier <quentin.forestier@heig-vd.ch>
2021-04-01 11:56:30 +0200

    Ajout de l'interpreteur markdown
    Les tests et les codes sont fait
    
    Signed-off-by: Quentin Forestier <quentin.forestier@heig-vd.ch>

--- src/main/java/gen/command/Build.java
+++ src/main/java/gen/command/Build.java
@@ -9,24 +9,214 @@
 
 package gen.command;
 
+import org.apache.commons.io.FilenameUtils;
+
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
+
+import org.apache.commons.io.FileUtils;
+
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.commonmark.Extension;
+import org.commonmark.ext.autolink.AutolinkExtension;
+import org.commonmark.ext.front.matter.YamlFrontMatterExtension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.ext.gfm.strikethrough.StrikethroughExtension;
+import org.commonmark.ext.image.attributes.ImageAttributesExtension;
+import org.commonmark.ext.ins.InsExtension;
+import org.commonmark.ext.task.list.items.TaskListItemsExtension;
+
+import org.commonmark.ext.front.matter.YamlFrontMatterVisitor;
+
 
 /**
  * Class implémentant la sous commande build.
- * @author Herzig Melvyn
+ *
+ * @author Forestier Quentin, Herzig Melvyn
  */
 @Command(name = "build", description = "Build a static site")
 public class Build implements Callable<Integer>
 {
-  /**
-   * Méthode pour l'appel de la commande build.
-   */
-  @Override public Integer call()
-  {
-    System.out.printf("build");
-    return 0;
-  }
+    @CommandLine.Parameters(index = "0", description = "path to build",
+            defaultValue = "/")
+    String path;
 
+
+    private File root;
+    private File buildRoot;
+
+    private final LinkedList<Extension> extensions = new LinkedList<>();
+    private Parser parser;
+    private HtmlRenderer renderer;
+
+
+    /**
+     * Méthode pour l'appel de la commande build.
+     */
+    @Override
+    public Integer call()
+    {
+        Path p = Paths.get(path);
+
+        String pathStart = p.isAbsolute() ? "" : ".";
+        root = new File(pathStart + path);
+
+        buildFiles();
+
+        return 0;
+    }
+
+    /**
+     * Créer le dossier build et génère les fichiers HTML
+     */
+    private void buildFiles()
+    {
+        try
+        {
+            buildRoot = new File(root.getAbsolutePath() + "/build");
+
+            FileUtils.deleteDirectory(buildRoot);
+
+            FileUtils.copyDirectory(root, buildRoot);
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        extensions.add(AutolinkExtension.create());
+        extensions.add(YamlFrontMatterExtension.create());
+        extensions.add(TablesExtension.create());
+        extensions.add(StrikethroughExtension.create());
+        extensions.add(ImageAttributesExtension.create());
+        extensions.add(InsExtension.create());
+        extensions.add(TaskListItemsExtension.create());
+
+        parser = Parser.builder().extensions(extensions).build();
+        renderer = HtmlRenderer.builder().extensions(extensions).escapeHtml(true).build();
+
+        for (File f : FileUtils.listFiles(new File(buildRoot.getAbsolutePath()), null, true))
+        {
+            createHTMLFile(f);
+        }
+
+
+    }
+
+    /**
+     * Transforme le contenu du markdown en balise html
+     *
+     * @param file Fichier markdown a transformer
+     * @return String contenu html
+     */
+    private String mdToHtml(File file)
+    {
+
+        YamlFrontMatterVisitor visitor = new YamlFrontMatterVisitor();
+
+
+        if (file.isFile())
+        {
+            if (FilenameUtils.getExtension(file.getName()).equals("md"))
+            {
+                try
+                {
+                    var doc =
+                            parser.parseReader(new InputStreamReader(Files.newInputStream(Path.of(file.getAbsolutePath()))));
+                    doc.accept(visitor);
+
+                    Map<String, List<String>> data = visitor.getData();
+                    return String.format("<!doctype html>\n" +
+                            "<html>\n" +
+                            "\t<head>\n" +
+                            "<meta charset=\"UTF-8\">\n" +
+                            "%s\n" +
+                            "\t</head>\n" +
+                            "\t<body>\n" +
+                            "%s\n" +
+                            "\t</body>\n" +
+                            "</html>", renderMetadata(data), renderer.render(doc));
+
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Créer le fichier html correspondant au fichier markdown en paramètre
+     *
+     * @param file Fichier markdown a transformer
+     */
+    private void createHTMLFile(File file)
+    {
+        String basename = FilenameUtils.getBaseName(file.getAbsolutePath());
+        String htmlContent = mdToHtml(file);
+        if (htmlContent == null)
+        {
+            return;
+        }
+
+        try
+        {
+            File htmlFile = new File(file.getParentFile() + "/" + basename + ".html");
+            htmlFile.createNewFile();
+            FileWriter writer = new FileWriter(htmlFile);
+
+
+            writer.write(htmlContent);
+            writer.close();
+            file.delete();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Génère les meta données à partir d'une map
+     *
+     * @param data Map contenant les clés/valeurs des méta données
+     * @return String contenant toutes les balises <meta>
+     */
+    private String renderMetadata(Map<String, List<String>> data)
+    {
+        StringBuilder metadata = new StringBuilder();
+        for (Map.Entry<String, List<String>> d : data.entrySet())
+        {
+            StringBuilder values = new StringBuilder();
+            for (int i = 0; i < d.getValue().size(); ++i)
+            {
+                if (i != 0)
+                {
+                    values.append(", ");
+                }
+
+                values.append(d.getValue().get(i));
+            }
+            metadata.append(String.format("<meta name=\"%s\" content=\"%s\">\n", d.getKey(), values));
+        }
+
+        return metadata.toString();
+    }
 }
---
+++ src/test/java/gen/command/BuildTest.java
@@ -0,0 +1,239 @@
+/*
+ -----------------------------------------------------------------------------------
+ Cours       : Génie logiciel (GEN)
+ Fichier     : command.InitTest
+ Auteur(s)   : Forestier Quentin & Melvyn Herzig
+ Date        : 06.03.2021
+ -----------------------------------------------------------------------------------
+ */
+
+package gen.command;
+
+import gen.Statique;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Classe de test pour la commande init
+ */
+public class BuildTest
+{
+    private static final String path = "/buildtest";
+
+    /**
+     * Test si la commande build peut créer le dossier build ainsi que les fichiers html
+     */
+    @Test
+    void runTestEmptyPath()
+    {
+        // Préparation de la commande.
+        Callable<Integer> callable = new Statique();
+        CommandLine cmd = new CommandLine(callable);
+
+        String[] args = new String[]{"init"};
+
+        cmd.execute(args);
+
+        args = new String[]{"build"};
+
+        cmd.execute(args);
+
+        File index = new File("./build/index.html");
+
+        String fullpath = index.getAbsolutePath();
+        assertTrue(index.exists());
+
+    }
+
+    /**
+     * Test si la commande build peut créer le dossier build ainsi que les fichiers html avec un chemin spécifié
+     */
+    @Test
+    void runTestWithPath()
+    {
+        // Préparation de la commande.
+        Callable<Integer> callable = new Statique();
+        CommandLine cmd = new CommandLine(callable);
+
+        String[] args = new String[]{"init", path};
+
+        cmd.execute(args);
+
+        args = new String[]{"build", path};
+
+        cmd.execute(args);
+
+        File index = new File("./" + path + "/build/index.html");
+
+        assertTrue(index.exists());
+    }
+
+    /**
+     * Test si la conversion du markdown au html fonctionne
+     * Toutes les traductions Markdown -> HTML ont été testée ici
+     */
+    @Test
+    void runTestMarkdownToHtml()
+    {
+        // Préparation de la commande.
+        Callable<Integer> callable = new Statique();
+        CommandLine cmd = new CommandLine(callable);
+
+        String[] args = new String[]{"init", path};
+
+        cmd.execute(args);
+        try
+        {
+            FileWriter writer = new FileWriter("./" + path + "/index.md");
+            writer.write("---\n" +
+                    "titre: Article de test\n" +
+                    "auteur: Quentin Forestier\n" +
+                    "date: 2021-03-25\n" +
+                    "languages:\n" +
+                    " - html\n" +
+                    " - css\n" +
+                    "---\n" +
+                    "# Un titre\n" +
+                    "## Un sous-titre\n" +
+                    "Notre projet github [ici](https://github.com/gen-classroom/projet-berney_forestier_herzig) \n" +
+                    "\n" +
+                    "Une autre page de mon site [ici](./subdir/subdirfile.html)\n" +
+                    "\n" +
+                    "| col1 | col2 |\n" +
+                    "|---|---|\n" +
+                    "|cell1|cell2|\n" +
+                    "\n" +
+                    "~~du texte barré~~\n" +
+                    "\n" +
+                    "++du texte soulingé++\n" +
+                    "\n" +
+                    "Qu'est-ce qui est mieux ? \n" +
+                    "- [ ]  Ne pas avoir des checkbox\n" +
+                    "- [x] Avoir des checkbox\n" +
+                    "\n" +
+                    "\n" +
+                    "---\n" +
+                    "\n" +
+                    "Une jolie image pour vos beaux yeux.\n" +
+                    "\n" +
+                    "![Une image](./images/image.png)\n" +
+                    "\n" +
+                    "\n" +
+                    "La même image en plus petite\n" +
+                    "\n" +
+                    "![Une image plus petite](./images/image.png){width=200}");
+
+            writer.close();
+
+            args = new String[]{"build", path};
+            cmd.execute(args);
+
+            File file = new File("./" + path + "/build/index.html");
+            String data = FileUtils.readFileToString(file, "UTF-8");
+
+            String expectedOutput = "<!doctype html>\n" +
+                    "<html>\n" +
+                    "\t<head>\n" +
+                    "<meta charset=\"UTF-8\">\n" +
+                    "<meta name=\"titre\" content=\"Article de test\">\n" +
+                    "<meta name=\"auteur\" content=\"Quentin Forestier\">\n" +
+                    "<meta name=\"date\" content=\"2021-03-25\">\n" +
+                    "<meta name=\"languages\" content=\"html, css\">\n" +
+                    "\n" +
+                    "\t</head>\n" +
+                    "\t<body>\n" +
+                    "<h1>Un titre</h1>\n" +
+                    "<h2>Un sous-titre</h2>\n" +
+                    "<p>Notre projet github <a href=\"https://github.com/gen-classroom/projet-berney_forestier_herzig\">ici</a></p>\n" +
+                    "<p>Une autre page de mon site <a href=\"./subdir/subdirfile.html\">ici</a></p>\n" +
+                    "<table>\n" +
+                    "<thead>\n" +
+                    "<tr>\n" +
+                    "<th>col1</th>\n" +
+                    "<th>col2</th>\n" +
+                    "</tr>\n" +
+                    "</thead>\n" +
+                    "<tbody>\n" +
+                    "<tr>\n" +
+                    "<td>cell1</td>\n" +
+                    "<td>cell2</td>\n" +
+                    "</tr>\n" +
+                    "</tbody>\n" +
+                    "</table>\n" +
+                    "<p><del>du texte barré</del></p>\n" +
+                    "<p><ins>du texte soulingé</ins></p>\n" +
+                    "<p>Qu'est-ce qui est mieux ?</p>\n" +
+                    "<ul>\n" +
+                    "<li><input type=\"checkbox\" disabled=\"\"> Ne pas avoir des checkbox</li>\n" +
+                    "<li><input type=\"checkbox\" disabled=\"\" checked=\"\"> Avoir des checkbox</li>\n" +
+                    "</ul>\n" +
+                    "<hr />\n" +
+                    "<p>Une jolie image pour vos beaux yeux.</p>\n" +
+                    "<p><img src=\"./images/image.png\" alt=\"Une image\" /></p>\n" +
+                    "<p>La même image en plus petite</p>\n" +
+                    "<p><img src=\"./images/image.png\" alt=\"Une image plus petite\" width=\"200\" /></p>\n" +
+                    "\n" +
+                    "\t</body>\n" +
+                    "</html>";
+            assertEquals(expectedOutput, data.trim());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Supprimer les fichiers à la fin des tests
+     */
+
+    @AfterAll
+    static void clean()
+    {
+        try
+        {
+
+            File index = new File("./index.md");
+            File config = new File("./config.yaml");
+            if (index.exists())
+            {
+                index.delete();
+            }
+            if (config.exists())
+            {
+                config.delete();
+            }
+            File dir = new File("./build");
+            if (dir.exists())
+            {
+                FileUtils.deleteDirectory(dir);
+            }
+
+            // Nettoie après le test runTestWithPath
+
+            dir = new File("./" + path);
+            if (dir.exists())
+            {
+                FileUtils.deleteDirectory(dir);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+}
